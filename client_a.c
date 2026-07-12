49eec7e213130f8bf454baf2f9ab344e9259bf78
Vasu0521c <vasudevanoffline@hotmail.com>
2026-07-12 22:04:21 +0530

    New project Chatbot

---
+++ client_a.c
@@ -0,0 +1,19 @@
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <stdint.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main() {
+    
+    uint16_t port = htons(5000);
+    struct sockaddr_in client;
+
+    
+
+    
+    return 0;
+}

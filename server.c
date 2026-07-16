c571c419d91023ab5d28a2ef83988683928e4e95
Vasu0521c <vasudevanoffline@hotmail.com>
2026-07-16 14:38:40 +0530

    complete working chatbot

--- server.c
+++ server.c
@@ -1,45 +1,88 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/epoll.h>
-#include <stdint.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-  
-typedef uint16_t bytes;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <string.h>
+#include <errno.h>
 
-int main() {
+#define MAX_MSG_SIZE 1025
 
-    int the_sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr, client;
-    fd_set ready_set;
-    addr.sin_port   = htons(17112);
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    int length = sizeof(addr);
+int main()
+{
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    int binding_status = bind(the_sock, (struct sockaddr *) &addr, length);
-    if(binding_status == 0) {
-        printf("socket binding successfull\n");
-    }
-    else {
-        printf("socket bindin failed\n");
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(17112);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int binding_status = bind(server_fd, (struct sockaddr *)&server, sizeof(server));
+    if(binding_status == -1) {
+        printf("binding failed, errno : %d\n", errno);
         exit(1);
     }
-
-    int epo = epoll_create(1);
-    struct epoll_event eve;
-    eve.data.fd = the_sock;
-    epoll_ctl(epo, EPOLL_CTL_ADD , the_sock,);
-
-    while(1) {
-        char message[10000] = {};
-        read(cli_soc, message, 9999);
-        printf("The client says : %s",message);
-        write(cli_soc, "message received", strlen("message received"));
+    int listen_status = listen(server_fd, 10);
+    if(listen_status != 0) {
+        printf("listening failed\n");
+        exit(1);
     }
+    /* printf("listening on : %s  %d", server.sin_addr.s_addr, server.sin_port); */
+    printf("listening on\n");
 
+    int epfd = epoll_create1(0);
+
+    struct epoll_event ev, events[10];
+
+    ev.events = EPOLLIN;
+    ev.data.fd = server_fd;
+
+    epoll_ctl(epfd, EPOLL_CTL_ADD, server_fd, &ev);
+
+    char buffer[MAX_MSG_SIZE];
+
+    while (1)
+    {
+        int n = epoll_wait(epfd, events, 10, -1);
+        if(n == -1) {
+            printf("epoll waiting sequence failed\n");
+            exit(1);
+        }
+        memset(buffer, '\0', 1023);
+
+        for(int i = 0; i < n; i++) {
+            int fd = events[i].data.fd;
+
+            if(fd == server_fd) {
+                int client = accept(server_fd, NULL, NULL);
+                if(client == -1) {
+                    printf("connection acceptance failed\n");
+                    continue;
+                }
+                printf("connection acceptanced\n");
+                ev.events = EPOLLIN;
+                ev.data.fd = client;
+                int ctl_status = epoll_ctl(epfd, EPOLL_CTL_ADD, client, &ev);
+                if(ctl_status != 0) {
+                    printf("epol control add sequence failed\n");
+                    continue;
+                }
+                printf("epol control add sequence successfull\n");
+                printf("Client %d connected\n", client);
+            }
+            else {
+                int bytes = read(fd, buffer, sizeof(buffer));
+                printf("%s\n", buffer);
+                if(bytes <= 0) {
+                    epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+                    close(fd);
+                    printf("Client %d disconnected\n", fd);
+                }
+                else {
+                    write(fd, "messafge received", strlen("messafge received"));
+                }
+            }
+        }
+    }
     return 0;
 }
